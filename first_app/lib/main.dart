9cba3e99af0a0fd80190a8aa78ed7582f9b319ab
F.J.F Situmeang <situmeang92.fs@gmail.com>
2021-07-09 23:47:13 +0700

    First Commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './widgets/user_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Expanse App'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('this Chart'),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ first_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 10,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+              ),
+              FlatButton(
+                onPressed: () {
+                  print(titleController.text);
+                  print(amountController.text);
+                  addTx(titleController.text,
+                      double.parse(amountController.text));
+                },
+                child: Text("Add Transaction"),
+                textColor: Colors.purple,
+              )
+            ],
+          ),
+        ));
+  }
+}
